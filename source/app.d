c7fda8f9995221e388f06f3f202f7918a6da6306
ray45422 <ray45422@gmail.com>
2016-08-27 18:34:41 +0900

    add game status

--- source/app.d
+++ source/app.d
@@ -1,13 +1,29 @@
 static import ar = armos;
 
+enum GameStatus{
+    Opening,
+    Guide,
+    Playing,
+    PreGameover,
+    Gameover,
+}
+
 class TestApp : ar.app.BaseApp{
     this(){}
 
-    override void setup(){}
+    override void setup(){
+        _state = GameStatus.Opening;
+    }
 
     override void update(){}
 
-    override void draw(){}
+    override void draw(){
+        if(_state == GameStatus.Opening){}
+        if(_state == GameStatus.Guide){}
+        if(_state == GameStatus.Playing){}
+        if(_state == GameStatus.PreGameover){}
+        if(_state == GameStatus.Gameover){}
+    }
 
     override void keyPressed(ar.utils.KeyType key){}
 
@@ -15,9 +31,21 @@ class TestApp : ar.app.BaseApp{
 
     override void mouseMoved(ar.math.Vector2i position, int button){}
 
-    override void mousePressed(ar.math.Vector2i position, int button){}
+    override void mousePressed(ar.math.Vector2i position, int button){
+        if(_state == GameStatus.Playing){}
+        if(_state == GameStatus.Opening){
+            _state = GameStatus.Guide;
+        }
+        if(_state == GameStatus.Guide){}
+        if(_state == GameStatus.PreGameover){}
+        if(_state == GameStatus.Gameover){
+            _state = GameStatus.Opening;
+        }
+    }
 
     override void mouseReleased(ar.math.Vector2i position, int button){}
+
+    private GameStatus _state;
 }
 
 void main(){ar.app.run(new TestApp);}
